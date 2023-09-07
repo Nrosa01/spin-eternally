aba1f8d5c804bbe84789130534363316a2872155
Rioni <nrosa01@ucm.es>
2023-09-07 19:01:08 +0200

    Script sin parámetros de transform

--- Assets/Scripts/SquashAndStretch.cs
+++ Assets/Scripts/SquashAndStretch.cs
@@ -2,24 +2,24 @@ using UnityEngine;
 
 public class SquashAndStretch : MonoBehaviour
 {
-    public Transform cube;
-    public Transform parent;
     Vector3 previousPosition;
     public float maxSpeedThreshold;
     public float minSpeedThreshold;
     public float maxStretch = 1.8f;
 
     float inverseStretch;
+    private Transform parent;
 
     private void Start()
     {
         inverseStretch = 1 / Mathf.Sqrt(maxStretch);
+        parent = transform.parent;
     }
 
     void Stretch()
     {
-        Vector3 velocity = (cube.position - previousPosition) / Time.deltaTime;
-        previousPosition = cube.position;
+        Vector3 velocity = (transform.position - previousPosition) / Time.deltaTime;
+        previousPosition = transform.position;
         
         float squashFactor = Mathf.Clamp01(velocity.magnitude / maxSpeedThreshold);
         parent.localScale = Vector3.Lerp(Vector3.one, new Vector3(inverseStretch, maxStretch, inverseStretch), squashFactor);
